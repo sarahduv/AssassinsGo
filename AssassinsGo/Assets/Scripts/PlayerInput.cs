ff724a30b5047ea2e2238daa7ef42318ccfd9a48
sarahduv <46334082+sarahduv@users.noreply.github.com>
2020-03-16 09:38:38 -0700

    changed playerInput to properties

--- AssassinsGo/Assets/Scripts/PlayerInput.cs
+++ AssassinsGo/Assets/Scripts/PlayerInput.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    public float h;
-    public float v;
-    public bool inputEnabled = true;
+    float m_h;
+    public float H { get { return m_h; } }
+
+    float m_v;
+    public float V { get { return m_v; } }
+    
+    bool m_inputEnabled = false;
+    public bool InputEnabled { get { return m_inputEnabled;  } set { m_inputEnabled = value; } }
 
     public void GetKeyInput()
     {
-        if (inputEnabled)
+        if (m_inputEnabled)
         {
-            h = Input.GetAxisRaw("Horizontal");
-            v = Input.GetAxisRaw("Vertical");
+            m_h = Input.GetAxisRaw("Horizontal");
+            m_v = Input.GetAxisRaw("Vertical");
         }              
     }
 }
