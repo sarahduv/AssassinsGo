1c2f44ec1a2b016043bec78cdcdff8b1bdd75fb0
sarahduv <46334082+sarahduv@users.noreply.github.com>
2020-03-16 16:49:18 -0700

    adding node itween effects

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public GameObject geometry;
+    public float scaleTime = 0.3f;
+    public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
+    public bool autoRun;
+    public float delay = 1f;
+
+    void Start()
+    {
+        if (geometry != null)
+        {
+            geometry.transform.localScale = Vector3.zero;
+        }
+
+        if (autoRun)
+        {
+            ShowGeometry();
+        }
+    }
+
+    public void ShowGeometry()
+    {
+        if (geometry != null)
+        {
+            iTween.ScaleTo(geometry, iTween.Hash(
+                "time", scaleTime,
+                "scale", Vector3.one,
+                "easetype", easeType,
+                "delay", delay
+                ));
+        }
+    }
+}
