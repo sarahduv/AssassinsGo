63b85a349471916d44858e20beec037b93b044cd
sarahduv <46334082+sarahduv@users.noreply.github.com>
2020-03-17 10:43:40 -0700

    adding node coordinates

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    Vector2 m_coordinate;
+    public Vector2 Coordinate { get { return Utility.Vector2Round(m_coordinate); } }
+
+    List<Node> m_neighborNodes = new List<Node>();
+    public List<Node> NeighborNodes { get { return m_neighborNodes; } }
+
+    Board m_board;
+
+    // reference to mesh for display of node
     public GameObject geometry;
+
+    // time for scale animation to play
     public float scaleTime = 0.3f;
+
+    // ease in-out for animation
     public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
+
+    // do we activate the animation st start?
     public bool autoRun;
+
+    // delay time before animation
     public float delay = 1f;
 
+
+    private void Awake()
+    {
+        {
+            m_board = Object.FindObjectOfType<Board>();
+            m_coordinate = new Vector2(transform.position.x, transform.position.z);
+        }
+    }
     void Start()
     {
         if (geometry != null)
@@ -20,6 +45,11 @@ public class Node : MonoBehaviour
         if (autoRun)
         {
             ShowGeometry();
+        }
+
+        if (m_board != null)
+        {
+            m_neighborNodes = FindNeighbors(m_board.AllNodes);
         }
     }
 
@@ -34,5 +64,22 @@ public class Node : MonoBehaviour
                 "delay", delay
                 ));
         }
+    }
+
+    public List<Node> FindNeighbors(List<Node> nodes)
+    {
+        List<Node> nList = new List<Node>();
+
+        foreach (Vector2 dir in Board.directions)
+        {
+            Node foundNeighbor = nodes.Find(n => n.Coordinate == Coordinate + dir);
+            
+            if (foundNeighbor != null && !nList.Contains(foundNeighbor))
+            {
+                nList.Add(foundNeighbor);
+            }
+        }
+
+        return nList;
     }
 }
